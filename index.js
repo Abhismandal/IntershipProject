597fa0021686fc5c4cc49f807adb59b359c4bb03
9201 <mabhishek23it@student.mes.ac.in>
2026-02-02 00:52:21 +0530

    Add index.html

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = new Server(server);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index1.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // --- MongoDB setup ---
